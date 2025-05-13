0cd3a804524c1dcef010b2631b0f8dd5f0bbdd48
Kevin <kevin@Mac-mini-de-Kevin.local>
2025-05-13 21:21:30 +0200

    Initial commit

---
+++ IG-story/IG_storyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IG_storyApp.swift
+//  IG-story
+//
+//  Created by Kevin on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct IG_storyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IG-storyTests/IG_storyTests.swift
@@ -0,0 +1,17 @@
+//
+//  IG_storyTests.swift
+//  IG-storyTests
+//
+//  Created by Kevin on 13/05/2025.
+//
+
+import Testing
+@testable import IG_story
+
+struct IG_storyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
